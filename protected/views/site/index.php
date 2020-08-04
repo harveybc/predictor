29defbea690156df064d67563386397670a3819b
Harvey Bastidas <harveybc@users.noreply.github.com>
2020-08-04 17:13:52 -0500

    removed a lot of stuff for refactoring as a visualizer in python/flask

--- protected/views/site/index.php
+++
@@ -1,5 +0,0 @@
-<?php $this->pageTitle=Yii::app()->name; ?>
-
-<?php $this->setPageTitle ('Bienvenido a CBM - Cervecería del Valle'); ?>
-<p>Software de Administración de Base de Datos de CBM</p>
-<p>Trabajo en progreso</p>
--- protected/views/site/index_1.php
+++
@@ -1,6 +0,0 @@
-<?php $this->pageTitle=Yii::app()->name; ?>
-
-<?php $this->setPageTitle ('Bienvenido a SGDOC- Cervecería del Valle'); ?>
-<p>Software de Gestión de Documentación</p>
-<p>Trabajo en progreso</p>
-
