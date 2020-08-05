e1ba5ff99cfe49e09c94ca85184992ef92a191ce
Harvey Bastidas <harveybc@users.noreply.github.com>
2020-08-05 14:12:20 -0500

    Update visualizer.py

--- visualizer/visualizer.py
+++ visualizer/visualizer.py
@@ -7,7 +7,7 @@ import logging
 import numpy as np
 import csv
 import pkg_resources
-from visualizer_base import FeatureExtractorBase
+from visualizer.visualizer_base import FeatureExtractorBase
 
 # from visualizer import __version__
 
