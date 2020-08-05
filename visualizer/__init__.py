c70b3a0a10a408f54a93b336cca57fbd6e26c060
Harvey Bastidas <harveybc@users.noreply.github.com>
2020-08-05 17:40:40 -0500

    updated url_rules

--- visualizer/__init__.py
+++ visualizer/__init__.py
@@ -3,6 +3,8 @@ import json
 from flask import Flask
 from flask import Blueprint
 from visualizer.visualizer import FeatureExtractor
+from visualizer import auth
+from visualizer.routes import visualizer_blueprint 
 
 def read_plugin_config(vis_config_file=None):
     """ Read the pulgin configuration JSON file from a path, if its None, uses a default configuration """
@@ -55,24 +57,14 @@ def create_app(test_config=None):
 
     db.init_app(app)
 
-    # apply the blueprints to the app
-    from visualizer import auth
-    import routes 
-
-    
     # get the output plugin template folder
     plugin_folder = fe.ep_output.template_path(p_config)
-    # construct the blueprint 
+    # construct the blueprint with configurable plugin_folder
     vis_bp = visualizer_blueprint(plugin_folder)
-    
     # register the blueprints
     app.register_blueprint(auth.bp)
     app.register_blueprint(vis_bp) 
-
-    # make url_for('index') == url_for('blog.index')
-    # in another app, you might define a separate main index here with
-    # app.route, while giving the blog blueprint a url_prefix, but for
-    # the tutorial the blog will be the main index
+    # add the / route pointing to the index endpoint
     app.add_url_rule("/", endpoint="index")
 
     return app
