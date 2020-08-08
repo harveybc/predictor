b317f7c9d04167101c6c8fed149e13acd35fc917
Harvey Bastidas <harveybc@users.noreply.github.com>
2020-08-07 23:06:17 -0500

    Update user_seed.py

--- models/user_seed.py
+++ models/user_seed.py
@@ -1,6 +1,4 @@
-from flask.cli import with_appcontext
 from models.user import User
 
-@with_appcontext
 def user_seed():
     tmp =  User(username='test', password='pass', email='test@test.com').save()
