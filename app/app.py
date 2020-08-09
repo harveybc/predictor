fedbd4e73334f57381fa2552bd0a916129cf035f
Harvey <harveybc@ingeni-us.com>
2020-08-08 22:22:16 -0500

    updated app. py

--- app/app.py
+++ app/app.py
@@ -25,11 +25,11 @@ def configure_database(app):
     #@app.before_first_request
     def initialize_database():
     	print("Creating database")
-        from models.user import User 
+		from models.user import User 
         db.create_all()
         print("Seeding database")
-        from models.user_seed import user_seed
-        user_seed(app, db)
+		from models.user_seed import user_seed
+		user_seed(app, db)
 
     @app.teardown_request
     def shutdown_session(exception=None):
