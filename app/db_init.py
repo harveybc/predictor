db15d2dbbb332bc51ff9ba65d515d61079595316
Harvey Bastidas <harveybc@users.noreply.github.com>
2020-08-10 23:46:55 -0500

    Update db_init.py

--- app/db_init.py
+++ app/db_init.py
@@ -11,7 +11,7 @@ create_db(app)
 
 bp_init_db = Blueprint('init_db', __name__)
 # TODO: make DEBUG/PRODUCTION MODE  parametrizable
-app_config = config_dict['DEBUG']
+app_config = config_dict['Debug']
 app.config.from_object(config)
 
 @bp_init_db.cli.command('init_db')
