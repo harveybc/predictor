e876d27fd6752940899f679f176dc16062259018
Harvey Bastidas <harveybc@users.noreply.github.com>
2020-08-12 23:32:16 -0500

    updated seeding scripts

--- scripts/user_seed.py
+++ scripts/user_seed.py
@@ -1,6 +1,6 @@
 from models.user import User
 
-def user_seed(app, db):
+def seed(app, db):
     with app.app_context():
         tmp =  User(username='test', password='pass', email='test@test.com')
         db.session.add(tmp)
