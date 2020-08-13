2b716b60711e5cbceb95ce92d95d2361291052f3
Harvey Bastidas <harveybc@users.noreply.github.com>
2020-08-13 00:11:39 -0500

    Update data.sql

--- tests/data/data.sql
+++ tests/data/data.sql
@@ -1,13 +1,3 @@
--- Table: user
-INSERT INTO user (username, password)
-VALUES
-  ('test', 'pbkdf2:sha256:50000$TCI4GzcX$0de171a4f4dac32e3364c7ddc7c14f3e2fa61f2d17574483f7ffbb431b4acb2f'),
-  ('other', 'pbkdf2:sha256:50000$kJPKsz6N$d2d4784f1b030a9761f5ccaeeaca413f27f2ecb76d6168407af962ddce849f79');
--- Table: process
-INSERT INTO process (name, description,model_link, training_data_link, validation_data_link, user_id)
-VALUES
-  ('process0','d','ml','tdl','vdl',0),
-  ('process1','d','ml','tdl','vdl',1);
 
 PRAGMA foreign_keys = off;
 BEGIN TRANSACTION;
