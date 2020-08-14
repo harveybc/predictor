0d4a750ab912c4d3cc23c4a7386e3335e398b7a2
Harvey Bastidas <harveybc@users.noreply.github.com>
2020-08-14 00:24:18 -0500

    Update app.py

--- app/app.py
+++ app/app.py
@@ -17,7 +17,7 @@ def read_plugin_config(vis_config_file=None):
     if vis_config_file != None:
         file_path = vis_config_file
     else:
-        file_path = path.dirname(os.path.abspath(__file__)) + "//visualizer.json"
+        file_path = path.dirname(path.abspath(__file__)) + "//visualizer.json"
     with open(file_path) as f:
         data = json.load(f)
     return data
