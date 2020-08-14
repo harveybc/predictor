9b543b5fb596a70c918de3f03fbf552b23cbd690
Harvey Bastidas <harveybc@users.noreply.github.com>
2020-08-14 00:37:14 -0500

    Update routes.py

--- app/base/routes.py
+++ app/base/routes.py
@@ -46,7 +46,7 @@ def login():
     if not current_user.is_authenticated:
         return render_template( 'accounts/login.html',
                                 form=login_form)
-    return redirect(url_for('home_blueprint.index'))
+    return redirect(url_for('visualizer.index'))
 
 @blueprint.route('/register', methods=['GET', 'POST'])
 def register():
