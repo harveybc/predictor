a48face936af735cd5ab4941a0fcd8811df42943
Harvey Bastidas <harveybc@users.noreply.github.com>
2020-08-22 01:42:34 -0500

    Update user.py

--- models/seeds/user.py
+++ models/seeds/user.py
@@ -2,6 +2,6 @@ from models.user import User
 
 def seed(app, db):
     with app.app_context():
-        tmp =  User(username='test', password='pass', email='test@test.com')
+        tmp =  User(id=0, username='test', password='pass', email='test@test.com')
         db.session.add(tmp)
         db.session.commit()
