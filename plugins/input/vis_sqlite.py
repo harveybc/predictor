18a886f17753123d32ced75f0d9d34f5928c6574
Harvey Bastidas <harveybc@users.noreply.github.com>
2020-08-24 20:53:49 -0500

    Update vis_sqlite.py

--- plugins/input/vis_sqlite.py
+++ plugins/input/vis_sqlite.py
@@ -96,7 +96,7 @@ class VisSqlite(PluginBase):
             "SELECT " + original_field + ", " + predicted_field +
             " FROM " + table + 
             " WHERE process_id = " + str(process_id)
-        ).fetchone()
+        ).fetchall()
         result = dict(row)        
         return result
         
