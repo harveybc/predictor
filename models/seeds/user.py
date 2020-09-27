0cdd0908e5f7c5b37108c6aa6665891cc0fc09a8
Harvey Bastidas <harveybc@users.noreply.github.com>
2020-09-27 17:53:16 -0500

    Update user.py

--- models/seeds/user.py
+++ models/seeds/user.py
@@ -4,6 +4,6 @@ def seed(app, db):
     with app.app_context():
         tmp =  User(id=0, username='test', password='pass', admin=True, email='test@test.com')
         db.session.add(tmp)
-        tmp =  User(id=0, username='test2', password='pass2', admin=False, email='test2@test.com')
+        tmp =  User(id=1, username='test2', password='pass2', admin=False, email='test2@test.com')
         db.session.add(tmp)
         db.session.commit()
