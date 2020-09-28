6a74443e2b03be61ca840b3410344d268b5d57d2
Harvey Bastidas <harveybc@users.noreply.github.com>
2020-09-28 00:40:00 -0500

    updated user views

--- app/blueprints/user.py
+++ app/blueprints/user.py
@@ -22,7 +22,14 @@ def user_bp(plugin_folder):
     # construct the visualizer blueprint using the plugin folder as template folder
     bp = Blueprint("user_bp", __name__,  template_folder=plugin_folder)
 
-    @bp.route("/users")
+    @bp.route("/user")
+    @login_required
+    def user_index():
+        """Show the users index."""
+        user_list = current_app.config['FE'].ep_input.get_users()
+        return render_template("/plugin_templates/user/index.html", user_list = user_list)
+
+    @bp.route("/user/create")
     @login_required
     def user_index():
         """Show the users index."""
@@ -36,5 +43,40 @@ def user_bp(plugin_folder):
         user_list = current_app.config['FE'].ep_input.get_user_by_username(username)
         return render_template("/plugin_templates/user/detail.html", user_list =  user_list, username = username)
 
+    @bp.route("/<int:id>/update", methods=("GET", "POST"))
+    @login_required
+    def update(id):
+        """Update a post if the current user is the author."""
+        post = get_post(id)
+        if request.method == "POST":
+            title = request.form["title"]
+            body = request.form["body"]
+            error = None
+            if not title:
+                error = "Title is required."
+            if error is not None:
+                flash(error)
+            else:
+                db = get_db()
+                db.execute(
+                    "UPDATE post SET title = ?, body = ? WHERE id = ?", (title, body, id)
+                )
+                db.commit()
+                return redirect(url_for("visualizer.index"))
+        return render_template("visualizer/update.html", post=post)
+
+    @bp.route("/<int:id>/delete", methods=("POST",))
+    @login_required
+    def delete(id):
+        """Delete a post.
+
+        Ensures that the post exists and that the logged in user is the
+        author of the post.
+        """
+        get_post(id)
+        db = get_db()
+        db.execute("DELETE FROM post WHERE id = ?", (id,))
+        db.commit()
+        return redirect(url_for("visualizer.index"))    
  
     return bp
