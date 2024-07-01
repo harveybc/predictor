6768a547ac529c885072776cc01f544dfeb0adad
Harvey Bastidas <harveybc@users.noreply.github.com>
2024-07-01 15:05:02 -0500

    Update predictor_plugin_ann.py

--- app/plugins/predictor_plugin_ann.py
+++ app/plugins/predictor_plugin_ann.py
@@ -82,6 +82,7 @@ class Plugin:
     
     def calculate_mse(self, y_true, y_pred):
         print(f"Calculating MSE for shapes: y_true={y_true.shape}, y_pred={y_pred.shape}")
+        y_pred = y_pred.flatten()  # Ensure y_pred is a 1D array
         abs_difference = np.abs(np.array(y_true) - np.array(y_pred))
         squared_abs_difference = abs_difference ** 2
         mse = np.mean(squared_abs_difference)
@@ -90,6 +91,7 @@ class Plugin:
 
     def calculate_mae(self, y_true, y_pred):
         print(f"Calculating MAE for shapes: y_true={y_true.shape}, y_pred={y_pred.shape}")
+        y_pred = y_pred.flatten()  # Ensure y_pred is a 1D array
         abs_difference = np.abs(np.array(y_true) - np.array(y_pred))
         mae = np.mean(abs_difference)
         print(f"Calculated MAE: {mae}")
