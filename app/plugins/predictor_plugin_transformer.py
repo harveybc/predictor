d971c5e4749d7173cc434cd5f2b813aaa63d0aa6
Harvey Bastidas <harveybc@ingeni-us.com>
2024-12-22 02:43:33 -0500

    Update predictor_plugin_transformer.py

--- app/plugins/predictor_plugin_transformer.py
+++ app/plugins/predictor_plugin_transformer.py
@@ -13,8 +13,8 @@ class Plugin:
     plugin_params = {
         'epochs': 100,  # Increased number of epochs
         'batch_size': 128,
-        'intermediate_layers': 2,  
-        'initial_layer_size': 32,  
+        'intermediate_layers': 4,  
+        'initial_layer_size': 128,  
         'layer_size_divisor': 2,
         'num_heads': 4,  # Keeping the number of heads dependent on size as before
         'learning_rate': 0.0001,
