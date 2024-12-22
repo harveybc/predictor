5cee1f6c2f85c4620fc271e16d3390a10c0a99ba
Harvey Bastidas <harveybc@ingeni-us.com>
2024-12-22 02:42:07 -0500

    Update predictor_plugin_transformer.py

--- app/plugins/predictor_plugin_transformer.py
+++ app/plugins/predictor_plugin_transformer.py
@@ -16,7 +16,7 @@ class Plugin:
         'intermediate_layers': 2,  
         'initial_layer_size': 32,  
         'layer_size_divisor': 2,
-        'num_heads': 3,  # Keeping the number of heads dependent on size as before
+        'num_heads': 4,  # Keeping the number of heads dependent on size as before
         'learning_rate': 0.0001,
         'dropout_rate': 0.1
     }
