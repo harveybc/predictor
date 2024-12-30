c404017f5df9b353285812a6698152b6d82318ce
Harvey Bastidas <harveybc@ingeni-us.com>
2024-12-30 15:25:00 -0500

    update

--- app/plugins/predictor_plugin_cnn.py
+++ app/plugins/predictor_plugin_cnn.py
@@ -13,7 +13,7 @@ class Plugin:
     """
 
     plugin_params = {
-        'epochs': 10,
+        'epochs': 200,
         'batch_size': 128,
         'intermediate_layers': 3,
         'initial_layer_size': 64,
