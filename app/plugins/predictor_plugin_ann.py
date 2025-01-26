b34a8cd3d12b8ef9f14e26ec73db07f34ed165a2
Harvey Bastidas <harveybc@ingeni-us.com>
2025-01-26 05:40:20 -0500

    Update predictor_plugin_ann.py

--- app/plugins/predictor_plugin_ann.py
+++ app/plugins/predictor_plugin_ann.py
@@ -117,11 +117,12 @@ class Plugin:
                     kernel_regularizer=l2(l2_reg),
                     name=f"Dense_{size}"
                 )(x)
+
         
         # Add output Dense layer with linear activation for regression
         model_output = Dense(
             units=layers[-1],
-            activation='tanh',  # 'linear' activation for regression tasks
+            activation='linear',  # 'linear' activation for regression tasks
             kernel_initializer=GlorotUniform(),
             kernel_regularizer=l2(l2_reg),
             name="model_output"
