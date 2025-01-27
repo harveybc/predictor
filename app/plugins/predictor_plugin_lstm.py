ba5022171eb9f47ba2b2efd3a891f6f6a52258cc
Harvey Bastidas <harveybc@ingeni-us.com>
2025-01-27 00:55:25 -0500

    Update predictor_plugin_lstm.py

--- app/plugins/predictor_plugin_lstm.py
+++ app/plugins/predictor_plugin_lstm.py
@@ -66,7 +66,7 @@ class Plugin:
         x = model_input
         for size in layers[:-1]:
             if size > 1:
-                x = LSTM(size, activation='tanh', recurrent_activation='sigmoid', kernel_initializer=GlorotUniform(),kernel_regularizer=l2(self.params.get('l2_reg', 1e-4)), return_sequences=True)(x)
+                x = LSTM(size, activation='relu', recurrent_activation='sigmoid', kernel_initializer=GlorotUniform(),kernel_regularizer=l2(self.params.get('l2_reg', 1e-4)), return_sequences=True)(x)
                 #x = BatchNormalization()(x)
         x = LSTM(layers[-2], activation='tanh', recurrent_activation='sigmoid', kernel_initializer=GlorotUniform(), kernel_regularizer=l2(self.params.get('l2_reg', 1e-4)))(x)
         model_output = Dense(layers[-1], activation='linear', kernel_initializer=HeNormal(), kernel_regularizer=l2(self.params.get('l2_reg', 1e-4)), name="model_output")(x)
