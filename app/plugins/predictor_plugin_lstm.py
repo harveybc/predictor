659183cd67ea63c897134fcd2915dba4b3d6c448
Harvey Bastidas <harveybc@ingeni-us.com>
2025-01-27 00:54:03 -0500

    Update predictor_plugin_lstm.py

--- app/plugins/predictor_plugin_lstm.py
+++ app/plugins/predictor_plugin_lstm.py
@@ -66,12 +66,12 @@ class Plugin:
         x = model_input
         for size in layers[:-1]:
             if size > 1:
-                x = LSTM(size, activation='relu', recurrent_activation='relu', kernel_initializer=HeNormal(),kernel_regularizer=l2(self.params.get('l2_reg', 1e-4)), return_sequences=True)(x)
-                x = BatchNormalization()(x)
-        x = LSTM(layers[-2], activation='relu', recurrent_activation='relu', kernel_initializer=HeNormal(), kernel_regularizer=l2(self.params.get('l2_reg', 1e-4)))(x)
+                x = LSTM(size, activation='tanh', recurrent_activation='sigmoid', kernel_initializer=GlorotUniform(),kernel_regularizer=l2(self.params.get('l2_reg', 1e-4)), return_sequences=True)(x)
+                #x = BatchNormalization()(x)
+        x = LSTM(layers[-2], activation='tanh', recurrent_activation='signmoid', kernel_initializer=GlorotUniform(), kernel_regularizer=l2(self.params.get('l2_reg', 1e-4)))(x)
         model_output = Dense(layers[-1], activation='linear', kernel_initializer=HeNormal(), kernel_regularizer=l2(self.params.get('l2_reg', 1e-4)), name="model_output")(x)
         # add batch normalization
-        #model_output = BatchNormalization()(model_output)
+        model_output = BatchNormalization()(model_output)
 
 
         self.model = Model(inputs=model_input, outputs=model_output, name="predictor_model")
