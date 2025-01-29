aa6be92293d25d19f73b7910693826c3df117732
Harvey Bastidas <harveybc@ingeni-us.com>
2025-01-28 22:43:45 -0500

    Update data_processor.py

--- app/data_processor.py
+++ app/data_processor.py
@@ -358,6 +358,9 @@ def run_prediction_pipeline(config, plugin):
 
         except Exception as e:
             print(f"Iteration {iteration} failed with error:\n  {e}")
+            exc_type, exc_obj, exc_tb = sys.exc_info()
+            fname = os.path.split(exc_tb.tb_frame.f_code.co_filename)[1]
+            print(exc_type, fname, exc_tb.tb_lineno)
             continue  # Proceed to the next iteration even if one iteration fails
 
     # -----------------------
