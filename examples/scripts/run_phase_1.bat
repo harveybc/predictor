c8988978d30af36feeb9cdef35c9dd8f02646a18
Harvey Bastidas <harveybc@ingeni-us.com>
2025-01-30 01:07:57 -0500

    Update run_phase_1.bat

--- examples/scripts/run_phase_1.bat
+++ examples/scripts/run_phase_1.bat
@@ -4,9 +4,6 @@ setlocal enabledelayedexpansion
 :: Define the configuration directory
 set "CONFIG_DIR=examples\config\phase_1"
 
-:: Change to the script's directory (optional, in case it's run from elsewhere)
-cd /d "%~dp0"
-
 :: Iterate over all JSON files in the directory
 for %%F in ("%CONFIG_DIR%\*.json") do (
     echo Running preprocessor with configuration: %%~nxF
