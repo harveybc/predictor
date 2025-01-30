a88b0f20e7335e4de163a8fdd9c5ac631cd2893e
Harvey Bastidas <harveybc@ingeni-us.com>
2025-01-30 01:08:26 -0500

    Update run_phase_1.bat

--- examples/scripts/run_phase_1.bat
+++ examples/scripts/run_phase_1.bat
@@ -7,7 +7,7 @@ set "CONFIG_DIR=examples\config\phase_1"
 :: Iterate over all JSON files in the directory
 for %%F in ("%CONFIG_DIR%\*.json") do (
     echo Running preprocessor with configuration: %%~nxF
-    preprocessor.bat --load_config "%%F"
+    predictor.bat --load_config "%%F"
 )
 
 echo All configurations processed.
