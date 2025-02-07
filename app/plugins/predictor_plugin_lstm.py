a518d05e3ab958b717dbdc6754924bf88afa9cd8
Harvey Bastidas <harveybc@ingeni-us.com>
2025-02-06 22:52:31 -0500

    Update predictor_plugin_lstm.py

--- app/plugins/predictor_plugin_lstm.py
+++ app/plugins/predictor_plugin_lstm.py
@@ -21,7 +21,7 @@ class Plugin:
         'intermediate_layers':3,
         'initial_layer_size': 128,
         'layer_size_divisor': 2,
-        'learning_rate': 0.0001,
+        'learning_rate': 0.00001,
         'dropout_rate': 0.1,
         'patience': 15,
         'activation': 'tanh'
