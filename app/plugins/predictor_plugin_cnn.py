b8682d8dd6028932d6e22cbf9d748c623dbaa032
Harvey Bastidas <harveybc@ingeni-us.com>
2025-02-07 08:52:30 -0500

    Update predictor_plugin_cnn.py

--- app/plugins/predictor_plugin_cnn.py
+++ app/plugins/predictor_plugin_cnn.py
@@ -18,7 +18,7 @@ class Plugin:
         'intermediate_layers': 4,
         'initial_layer_size': 64,
         'layer_size_divisor': 2,
-        'learning_rate': 0.0001,
+        'learning_rate': 0.00002,
         'l2_reg': 1e-2,     # L2 regularization factor
         'activation': 'tanh'
     }
