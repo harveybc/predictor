0982011c29f02fb742b99a808727613410667853
Harvey Bastidas <harveybc@ingeni-us.com>
2025-02-08 06:57:45 -0500

    Update run_phase_1_base.bat

--- examples/scripts/run_phase_1_base.bat
+++ examples/scripts/run_phase_1_base.bat
@@ -4,10 +4,15 @@ set "CONFIG_DIR=examples\config\phase_1"
 :: Iterate over all JSON files in the directory
 
 predictor.bat --load_config %CONFIG_DIR%\phase_1_base_1575_1h_config.json
+
 predictor.bat --load_config %CONFIG_DIR%\phase_1_base_3150_1h_config.json
+
 predictor.bat --load_config %CONFIG_DIR%\phase_1_base_6300_1h_config.json
+
 predictor.bat --load_config %CONFIG_DIR%\phase_1_base_12600_1h_config.json
+
 predictor.bat --load_config %CONFIG_DIR%\phase_1_base_25200_1h_config.json
+
 
 
 echo All configurations processed.
