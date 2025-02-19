b9f7467f9b12738575739a3dbf4c5a63ac8e8d88
Harvey Bastidas <harveybc@ingeni-us.com>
2025-02-18 20:18:16 -0500

    Update predictor_plugin_ann.py

--- app/plugins/predictor_plugin_ann.py
+++ app/plugins/predictor_plugin_ann.py
@@ -26,7 +26,7 @@ class Plugin:
         'intermediate_layers': 3,
         'initial_layer_size': 64,
         'layer_size_divisor': 2,
-        'learning_rate': 0.0001,
+        'learning_rate': 0.00001,
         'activation': 'tanh',
         'l2_reg': 1e-2
     }
@@ -151,7 +151,7 @@ class Plugin:
         
         callbacks = []
         early_stopping_monitor = EarlyStopping(
-            monitor='val_loss',
+            monitor='loss',
             patience=self.params['patience'],
             restore_best_weights=True,
             verbose=1
