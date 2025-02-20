16d6d22971969701b9a71cb12fdc8e805b3d4905
Harvey Bastidas <harveybc@ingeni-us.com>
2025-02-19 23:12:34 -0500

    Update predictor_plugin_cnn_mmd.py

--- app/plugins/predictor_plugin_cnn_mmd.py
+++ app/plugins/predictor_plugin_cnn_mmd.py
@@ -208,7 +208,7 @@ class Plugin:
         callbacks = []
         patience = self.params.get('patience', 25)
         early_stopping_monitor = EarlyStopping(
-            monitor='val_loss',
+            monitor='val_mae',
             patience=patience,
             restore_best_weights=True,
             verbose=1
