0560773d3c82994d7abfee7c607f291e54a9dd3c
Harvey Bastidas <harveybc@users.noreply.github.com>
2025-02-25 01:10:05 -0500

    Update ls_pred.bat

--- ls_pred.bat
+++ ls_pred.bat
@@ -1,2 +1,2 @@
-call predictor --load_config examples\config\phase_2\phase_2_3_cnn_1h_config.json
-call predictor --load_config examples\config\phase_2_daily\phase_2_3_cnn_1d_config.json
+call examples\scripts\run_phase_2.bat
+call examples\scripts\run_phase_2_daily.bat
