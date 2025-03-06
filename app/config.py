9f4eb796b94341f3946a5e411f2b629193754cf7
Harvey Bastidas <harveybc@users.noreply.github.com>
2025-03-06 04:26:41 -0500

    Update config.py

--- app/config.py
+++ app/config.py
@@ -49,7 +49,7 @@ DEFAULT_VALUES = {
     'initial_layer_size': 48,
     'intermediate_layers': 3,
     'layer_size_divisor': 2,
-    'l2_reg': 5e-3,          # L2 regularization factor
+    'l2_reg': 2e-3,          # L2 regularization factor
     'early_patience': 32,           # Early stopping patience
     'max_steps_train': 20000,
     'max_steps_test': 20000,
