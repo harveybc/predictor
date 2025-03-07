4d5796452d5d756d2803905a7ea1ed9f6e226c21
Harvey Bastidas <harveybc@users.noreply.github.com>
2025-03-06 21:56:58 -0500

    Update predictor_plugin_cnn.py

--- app/plugins/predictor_plugin_cnn.py
+++ app/plugins/predictor_plugin_cnn.py
@@ -229,21 +229,12 @@ class Plugin:
             self.skip_connections.append(x)
             if idx < len(layers) - 2:
                 x = MaxPooling1D(pool_size=2, name=f"max_pool_{idx+1}")(x)
-        x = Conv1D(filters=1,
+        model_output = Conv1D(filters=1,
                         kernel_size=3,
                         activation=self.params['activation'],
                         kernel_initializer=HeNormal(),
                         padding='same',
                         kernel_regularizer=l2(l2_reg))(x)
-        x = BatchNormalization()(x)
-        x = Flatten(name="flatten")(x)
-        model_output = Dense(
-            units=layers[-1],
-            activation='linear',
-            kernel_initializer=GlorotUniform(),
-            kernel_regularizer=l2(l2_reg),
-            name="model_output"
-        )(x)
 
         self.model = Model(inputs=inputs, outputs=model_output, name="cnn_model")
         print("CNN Model Summary:")
