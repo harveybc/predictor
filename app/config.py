59e6ffceb2d4af06aab6b1c3897d309cb6bfd3bc
Harvey Bastidas <harveybc@users.noreply.github.com>
2025-03-07 02:25:48 -0500

    Update config.py

--- app/config.py
+++ app/config.py
@@ -46,8 +46,8 @@ DEFAULT_VALUES = {
     'learning_rate': 1e-3,
     'time_horizon': 6,
     'window_size': 192,  # Number of time steps in each window (e.g., 24 for daily patterns)
-    'initial_layer_size': 128,
-    'intermediate_layers': 4,
+    'initial_layer_size': 32,
+    'intermediate_layers': 2,
     'layer_size_divisor': 2,
     'l2_reg':1e-4,          # L2 regularization factor, best 1e-3 = TestMAE:0.0103 R2:0.9921
     'max_steps_train': 20000,
