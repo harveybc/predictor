afe595e617af55b86a5f192b7b93df66aeb9ec46
Harvey Bastidas <harveybc@ingeni-us.com>
2025-03-08 19:15:31 -0500

    update

--- app/config.py
+++ app/config.py
@@ -44,6 +44,7 @@ DEFAULT_VALUES = {
     'max_steps_test': 6300,
     'iterations': 3,
     'epochs': 1000,
-    'uncertainty_file': 'prediction_uncertainties.csv'
+    'uncertainty_file': 'prediction_uncertainties.csv',
+    'batch_size': 32
     
 }
