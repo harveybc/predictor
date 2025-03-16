fced0e3aedaf20b570feda22965997bbecbec038
Harvey Bastidas <harveybc@ingeni-us.com>
2025-03-15 23:47:19 -0500

    Update run_phase_1.sh

--- examples/scripts/run_phase_1.sh
+++ examples/scripts/run_phase_1.sh
@@ -4,7 +4,7 @@ CONFIG_DIR="examples/config/phase_1"
 
 for file in "$CONFIG_DIR"/*.json; do
     echo "Running preprocessor with configuration: $(basename "$file")"
-    ./predictor.sh --load_config "$file"
+    sh ./predictor.sh --load_config "$file"
 done
 
 echo "All configurations processed."
