e411837ec688b64633f0172fce7ba9e2de41ce29
Harvey Bastidas <harveybc@ingeni-us.com>
2025-03-22 17:34:18 -0500

    Update stl_preprocessor.py

--- preprocessor_plugins/stl_preprocessor.py
+++ preprocessor_plugins/stl_preprocessor.py
@@ -49,8 +49,8 @@ class PreprocessorPlugin:
         "window_size": 24,
         "time_horizon": 6,
         "use_returns": True,
-        "stl_period": 120,
-        "stl_window": 72,
+        "stl_period": 24,
+        "stl_window": 48,
         "stl_plot_file": "stl_plot.png",
         "pos_encoding_dim": 16
     }
