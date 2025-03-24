87a3ddd86341acb6f60c836ab9a394db74f6a499
Harvey Bastidas <harveybc@ingeni-us.com>
2025-03-24 09:53:55 -0500

    update

--- predictor_plugins/predictor_plugin_ann.py
+++ predictor_plugins/predictor_plugin_ann.py
@@ -132,8 +132,8 @@ def composite_loss(y_true, y_pred, mmd_lambda, sigma=1.0):
     
 
     # Compute the batch signed error to use as feedback
-    batch_signed_error =2*return_error # best 1
-    batch_std = 2*tf.math.reduce_mean(mag_true - mag_pred)
+    batch_signed_error =0.1*return_error # best 1
+    batch_std = 1*tf.math.reduce_mean(mag_true - mag_pred)
     #print(f"DEBUG: Batch signed error: {batch_signed_error}, Batch std: {batch_std}")
 
     # Update the global tf.Variable 'last_mae' using assign.
