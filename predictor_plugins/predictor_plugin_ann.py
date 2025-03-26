27814223b8de28f4af7bbb888f1eb88f0ef21cc7
harveybc <harveybc@ingeni-us.com>
2025-03-26 01:14:42 -0500

    Update predictor_plugin_ann.py

--- predictor_plugins/predictor_plugin_ann.py
+++ predictor_plugins/predictor_plugin_ann.py
@@ -33,7 +33,7 @@ from tensorflow.keras.initializers import GlorotUniform
 last_mae = tf.Variable(1.0, trainable=False, dtype=tf.float32)
 last_std = tf.Variable(0.0, trainable=False, dtype=tf.float32)
 intercept=tf.Variable(1e-8, trainable=False, dtype=tf.float32)
-p_control=tf.Variable(1, trainable=False, dtype=tf.float32)
+p_control=tf.Variable(10, trainable=False, dtype=tf.float32)
 d_control=tf.Variable(1, trainable=False, dtype=tf.float32)
 i_control=tf.Variable(1, trainable=False, dtype=tf.float32)
 
