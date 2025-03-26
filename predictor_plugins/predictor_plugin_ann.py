7d6b7ba093d28eb37ebff3b26b506ebf04f1b870
harveybc <harveybc@ingeni-us.com>
2025-03-25 21:57:47 -0500

    Update predictor_plugin_ann.py

--- predictor_plugins/predictor_plugin_ann.py
+++ predictor_plugins/predictor_plugin_ann.py
@@ -33,7 +33,7 @@ from tensorflow.keras.initializers import GlorotUniform
 last_mae = tf.Variable(1.0, trainable=False, dtype=tf.float32)
 last_std = tf.Variable(0.0, trainable=False, dtype=tf.float32)
 penalty_lambda=tf.Variable(0.00001, trainable=False, dtype=tf.float32)
-intercept=tf.Variable(0.1, trainable=False, dtype=tf.float32)
+intercept=tf.Variable(0.01, trainable=False, dtype=tf.float32)
 
 # ---------------------------
 # Custom Callbacks (same as before)
