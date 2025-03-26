db389347db2f4f885f9dde235e43c0ea58809fa8
harveybc <harveybc@ingeni-us.com>
2025-03-25 21:55:16 -0500

    Update predictor_plugin_ann.py

--- predictor_plugins/predictor_plugin_ann.py
+++ predictor_plugins/predictor_plugin_ann.py
@@ -32,7 +32,7 @@ from tensorflow.keras.initializers import GlorotUniform
 # Denine TensorFlow global variables(used from the composite loss function):
 last_mae = tf.Variable(1.0, trainable=False, dtype=tf.float32)
 last_std = tf.Variable(0.0, trainable=False, dtype=tf.float32)
-penalty_lambda=tf.Variable(0.01, trainable=False, dtype=tf.float32)
+penalty_lambda=tf.Variable(0.00001, trainable=False, dtype=tf.float32)
 intercept=tf.Variable(0.1, trainable=False, dtype=tf.float32)
 
 # ---------------------------
