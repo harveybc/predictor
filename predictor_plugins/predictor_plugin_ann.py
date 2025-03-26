8b2289961588c21c2ee2d82f6148581964ac3b45
harveybc <harveybc@ingeni-us.com>
2025-03-25 22:22:02 -0500

    Update predictor_plugin_ann.py

--- predictor_plugins/predictor_plugin_ann.py
+++ predictor_plugins/predictor_plugin_ann.py
@@ -146,11 +146,11 @@ def composite_loss(y_true, y_pred, mmd_lambda, sigma=1.0):
     # Update the global tf.Variable 'last_mae' using assign.
     with tf.control_dependencies([last_mae.assign(batch_signed_error)]):
         #total_loss = (penalty + 1.0) * (huber_loss_val + (mmd_lambda * mmd_loss_val))
-        total_loss = (penalty)*(huber_loss_val + mmd_lambda * mmd_loss_val)
+        total_loss = (penalty + huber_loss_val + mmd_lambda * mmd_loss_val)
     # Update the global tf.Variable 'last_std' using assign.
     with tf.control_dependencies([last_std.assign(batch_std)]):
         #total_loss = (penalty + 1.0) * (huber_loss_val + (mmd_lambda * mmd_loss_val))
-        total_loss = (penalty)*(huber_loss_val + mmd_lambda * mmd_loss_val)
+        total_loss = (penalty + huber_loss_val + mmd_lambda * mmd_loss_val)
 
     return total_loss
 
