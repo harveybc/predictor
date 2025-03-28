ea52ee3a446f3ad61d29aaa6395945cf2c5e3e50
harveybc <harveybc@ingeni-us.com>
2025-03-27 22:18:29 -0500

    Update predictor_plugin_ann.py

--- predictor_plugins/predictor_plugin_ann.py
+++ predictor_plugins/predictor_plugin_ann.py
@@ -180,7 +180,7 @@ def composite_loss(y_true, y_pred, mmd_lambda, sigma=1.0):
     def vertical_left_asymptote(value, center):
         res = tf.cond(tf.greater_equal(value, center),
             lambda: 1e1*tf.math.log(tf.abs(value - center))+7,
-            lambda: mse_loss_val*1e5 - 1
+            lambda: mse_loss_val*1e6 - 1
         )   
         return res
     
@@ -238,11 +238,11 @@ def composite_loss(y_true, y_pred, mmd_lambda, sigma=1.0):
     # Update global variables last_mae and last_std with control dependencies.
     with tf.control_dependencies([last_mae.assign(batch_signed_error)]):
         #total_loss = reward + penalty + 3e6*mae_loss_val+ 3e8*mse_loss_val + mmd_lambda * mmd_loss_val
-        total_loss = 1e1*mse_min + asymptote
+        total_loss = 1e3*mse_min + asymptote
         #total_loss = 1e2*mse_loss_val + slope
     with tf.control_dependencies([last_std.assign(batch_std)]):
         #total_loss = reward + penalty + 3e6*mae_loss_val+ 3e8*mse_loss_val + mmd_lambda * mmd_loss_val
-        total_loss = 1e1*mse_min + asymptote
+        total_loss = 1e3*mse_min + asymptote
         #total_loss = 1e2*mse_loss_val + slope
     return total_loss
 
