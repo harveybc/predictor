d2b42287b1d021d6771946ae98699d3a4e325582
harveybc <harveybc@ingeni-us.com>
2025-04-03 03:58:04 -0500

    Update predictor_plugin_cnn.py

--- predictor_plugins/predictor_plugin_cnn.py
+++ predictor_plugins/predictor_plugin_cnn.py
@@ -479,15 +479,17 @@ class Plugin:
                  #                          name=f"head_dense_{j+1}{branch_suffix}")(head_dense_output)
                 head_dense_output = Conv1D(
                         filters=merged_units, kernel_size=3, padding='same',
-                        activation=activation, kernel_regularizer=l2(l2_reg),
+                        activation=activation, 
+                        #kernel_regularizer=l2(l2_reg),
                         name=f"head_conv1d_{j+1}_{branch_suffix}")(head_dense_output)
-                # MaxPooling layer
-                head_dense_output = MaxPooling1D(pool_size=2, name=f"head_maxpool_{j+1}{branch_suffix}")(head_dense_output)
+            # MaxPooling layer
+            head_dense_output = MaxPooling1D(pool_size=2, name=f"head_maxpool_{j+1}{branch_suffix}")(head_dense_output)
             # collapse to 1D
             #head_dense_output =Flatten(name=f"head_flatten{branch_suffix}")(head_dense_output)
             head_dense_output = Conv1D( # TODO: ESTO FUNCIONÓ BIEN.
                         filters=1, kernel_size=3, padding='same',
-                        activation=activation, kernel_regularizer=l2(l2_reg),
+                        activation=activation, 
+                        #kernel_regularizer=l2(l2_reg),
                         name=f"head_conv1d_last{branch_suffix}")(head_dense_output)
             #head_dense_output = Dense(merged_units, activation=activation, kernel_regularizer=l2(l2_reg),
             #                          name=f"head_dense_final{branch_suffix}")(head_dense_output)
@@ -497,11 +499,11 @@ class Plugin:
             reshaped_for_lstm = head_dense_output
             # Apply Bidirectional LSTM
             # return_sequences=False gives output shape (batch, 2 * lstm_units)
-            lstm_output =Flatten(name=f"head_last_flatten{branch_suffix}")(reshaped_for_lstm)
+            #lstm_output =Flatten(name=f"head_last_flatten{branch_suffix}")(reshaped_for_lstm)
             
-            #lstm_output = Bidirectional(
-            #    LSTM(lstm_units, return_sequences=False), name=f"bidir_lstm{branch_suffix}"
-            #)(reshaped_for_lstm)
+            lstm_output = Bidirectional(
+                LSTM(lstm_units, return_sequences=False), name=f"bidir_lstm{branch_suffix}"
+            )(reshaped_for_lstm)
 
             # --- Bayesian / Bias Layers ---
 
