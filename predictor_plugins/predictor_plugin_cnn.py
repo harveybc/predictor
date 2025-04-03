a593d2d11f2d37488695d8fb8beda0e4241b013a
harveybc <harveybc@ingeni-us.com>
2025-04-03 04:20:40 -0500

    update

--- predictor_plugins/predictor_plugin_cnn.py
+++ predictor_plugins/predictor_plugin_cnn.py
@@ -475,7 +475,7 @@ class Plugin:
             # --- Head Intermediate Dense Layers ---
             head_dense_output = merged
             for j in range(num_head_intermediate_layers):
-                 #head_dense_output = Dense(merged_units, activation=activation, kernel_regularizer=l2(l2_reg),
+                 #head_dense_output = DeSnse(merged_units, activation=activation, kernel_regularizer=l2(l2_reg),
                  #                          name=f"head_dense_{j+1}{branch_suffix}")(head_dense_output)
                 head_dense_output = Conv1D(
                         filters=merged_units//((j+1)*2), kernel_size=3, padding='same',
@@ -486,11 +486,11 @@ class Plugin:
                 head_dense_output = MaxPooling1D(pool_size=2, name=f"head_maxpool_{j+1}{branch_suffix}")(head_dense_output)
             # collapse to 1D
             #head_dense_output =Flatten(name=f"head_flatten{branch_suffix}")(head_dense_output)
-            head_dense_output = Conv1D( # TODO: ESTO FUNCIONÓ BIEN.
-                        filters=1, kernel_size=3, padding='same',
-                        activation=activation, 
-                        #kernel_regularizer=l2(l2_reg),
-                        name=f"head_conv1d_last{branch_suffix}")(head_dense_output)
+            #head_dense_output = Conv1D( # TODO: ESTO FUNCIONÓ BIEN.
+            #            filters=1, kernel_size=3, padding='same',
+            #            activation=activation, 
+            #            #kernel_regularizer=l2(l2_reg),
+            #            name=f"head_conv1d_last{branch_suffix}")(head_dense_output)
             #head_dense_output = Dense(merged_units, activation=activation, kernel_regularizer=l2(l2_reg),
             #                          name=f"head_dense_final{branch_suffix}")(head_dense_output)
             # --- Add BiLSTM Layer ---
