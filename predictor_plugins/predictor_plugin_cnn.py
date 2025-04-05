50927ffce347e0bd108ec62a3267a6e4ca20a767
Harvey Bastidas <harveybc@users.noreply.github.com>
2025-04-04 23:59:51 -0500

    Update predictor_plugin_cnn.py

--- predictor_plugins/predictor_plugin_cnn.py
+++ predictor_plugins/predictor_plugin_cnn.py
@@ -438,7 +438,7 @@ class Plugin:
                 x = Conv1D(filters=branch_units, kernel_size=1, padding='same', kernel_regularizer=l2(l2_reg),
                           name=f"feature_{c+1}_conv1d_{i+1}")(x)
                 # Max pooling
-                #x = MaxPooling1D(pool_size=2, strides=2, padding='same', name=f"feature_{c+1}_maxpooling_{i+1}")(x)
+                x = MaxPooling1D(pool_size=2, strides=2, padding='same', name=f"feature_{c+1}_maxpooling_{i+1}")(x)
             x = Conv1D(filters=1, kernel_size=1, padding='same', kernel_regularizer=l2(l2_reg),
                           name=f"feature_{c+1}_last_conv1d")(x)
             #x = MaxPooling1D(pool_size=2, strides=2, padding='same', name=f"feature_{c+1}_last_maxpooling")(x)
@@ -477,6 +477,8 @@ class Plugin:
             for j in range(num_intermediate_layers):
                 head_dense_output = Conv1D(filters=merged_units, kernel_size=3, padding='same', kernel_regularizer=l2(l2_reg),
                             name=f"merge_head{branch_suffix}_conv1d_{j+1}")(head_dense_output)
+                # Max pooling
+                head_dense_output = MaxPooling1D(pool_size=2, strides=2, padding='same', name=f"merge_head{branch_suffix}_maxpooling_{j+1}")(head_dense_output)
             head_dense_output = Conv1D(filters=1, kernel_size=1, padding='same', kernel_regularizer=l2(l2_reg),
                         name=f"merge_head{branch_suffix}_last_conv1d")(head_dense_output)
                 
