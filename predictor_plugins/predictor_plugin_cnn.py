ade981832577b03fb6edde3079fcc404ef439884
Harvey Bastidas <harveybc@users.noreply.github.com>
2025-04-04 21:03:52 -0500

    Update predictor_plugin_cnn.py

--- predictor_plugins/predictor_plugin_cnn.py
+++ predictor_plugins/predictor_plugin_cnn.py
@@ -462,12 +462,14 @@ class Plugin:
 
             # --- Head Intermediate Dense Layers ---
             head_dense_output = merged
-
+            for j in range(num_head_intermediate_layers):
+                 head_dense_output = Dense(merged_units, activation=activation, kernel_regularizer=l2(l2_reg),
+                                           name=f"head_dense_{j+1}{branch_suffix}")(head_dense_output)
 
             # --- Add BiLSTM Layer ---
             # Reshape Dense output to add time step dimension: (batch, 1, merged_units)
             # TODO: probar (batch, merged_units, 1)
-            reshaped_for_lstm = Reshape((1, merged_units), name=f"reshape_lstm{branch_suffix}")(head_dense_output)
+            reshaped_for_lstm = Reshape((merged_units, 1), name=f"reshape_lstm{branch_suffix}")(head_dense_output)
             reshaped_for_lstm = Conv1D(filters=merged_units, kernel_size=1, padding='same', kernel_regularizer=l2(l2_reg), name=f"conv1d_1{branch_suffix}")(reshaped_for_lstm)
             reshaped_for_lstm = Conv1D(filters=branch_units, kernel_size=1, padding='same', kernel_regularizer=l2(l2_reg), name=f"conv1d_2{branch_suffix}")(reshaped_for_lstm)
             # Apply Bidirectional LSTM
