0292455978ab2c439d8667de26b497ac9a449c59
Harvey Bastidas <harveybc@users.noreply.github.com>
2025-04-06 21:30:03 -0500

    update

--- examples/scripts/run_phase_1_transformer_daily.sh
+++ examples/scripts/run_phase_1_transformer_daily.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 # Define the configuration directory
-CONFIG_DIR="examples/config/phase_1"
+CONFIG_DIR="examples/config/phase_1_daily"
 
 # Process the JSON configuration files
-sh predictor.sh  --load_config "$CONFIG_DIR/phase_1_transformer_1575_1h_config.json"
-sh predictor.sh  --load_config "$CONFIG_DIR/phase_1_transformer_3150_1h_config.json"
-sh predictor.sh  --load_config "$CONFIG_DIR/phase_1_transformer_6300_1h_config.json"
-sh predictor.sh  --load_config "$CONFIG_DIR/phase_1_transformer_12600_1h_config.json"
-sh predictor.sh  --load_config "$CONFIG_DIR/phase_1_transformer_25200_1h_config.json"
+sh predictor.sh  --load_config "$CONFIG_DIR/phase_1_transformer_1575_1d_config.json"
+sh predictor.sh  --load_config "$CONFIG_DIR/phase_1_transformer_3150_1d_config.json"
+sh predictor.sh  --load_config "$CONFIG_DIR/phase_1_transformer_6300_1d_config.json"
+sh predictor.sh  --load_config "$CONFIG_DIR/phase_1_transformer_12600_1d_config.json"
+sh predictor.sh  --load_config "$CONFIG_DIR/phase_1_transformer_25200_1d_config.json"
 
-echo All hourly configurations processed.
+echo All daily configurations processed.
