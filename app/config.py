70a43dc4ecea474c1512440d8a0c1d1de1fd070c
Harvey Bastidas <harveybc@users.noreply.github.com>
2025-04-10 01:27:31 -0500

    update

--- app/config.py
+++ app/config.py
@@ -38,11 +38,7 @@ DEFAULT_VALUES = {
     'input_offset': 0,
     'window_size': 256,  # Number of time steps in each window (e.g., 24 for daily patterns)
     'l2_reg': 1e-4,          # L2 regularization factor
-<<<<<<< HEAD
-    'early_patience': 30,           # Early stopping patience
-=======
     'patience': 60,           # Early stopping patience
->>>>>>> master
     'max_steps_train': 6300,
     'max_steps_val': 6300,
     'max_steps_test': 6300,
