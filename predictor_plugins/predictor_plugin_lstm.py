9641b07af4968f67a26068e474a2da3f18168a3b
Harvey Bastidas <harveybc@users.noreply.github.com>
2025-04-19 02:24:54 -0500

    Update predictor_plugin_lstm.py

--- predictor_plugins/predictor_plugin_lstm.py
+++ predictor_plugins/predictor_plugin_lstm.py
@@ -446,13 +446,13 @@ class Plugin:
         # --- End Self-Attention Block ---
         x = Bidirectional(LSTM(merged_units, return_sequences=True, 
                     name=f"feature_lstm_1"))(x)
-        x = AveragePooling1D(pool_size=3, strides=2, name=f"pooling_1")(x)
+        x = AveragePooling1D(pool_size=3, strides=2, kernel_regularizer=l2(l2_reg), name=f"pooling_1")(x)
         #x = Bidirectional(LSTM(branch_units, return_sequences=True,
         #            name=f"feature_lstm_2"))(x)
         #x = AveragePooling1D(pool_size=3, strides=2, name=f"pooling_2")(x)
         
         # --- End Self-Attention Block ---
-        x = Bidirectional(LSTM(lstm_units, return_sequences=False,
+        x = Bidirectional(LSTM(lstm_units, return_sequences=False, kernel_regularizer=l2(l2_reg),
                     name=f"feature_lstm_3"))(x)
 
         
