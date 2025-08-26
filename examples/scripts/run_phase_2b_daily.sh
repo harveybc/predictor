006e1635d1bdc1b980b5a0f602e0f326ce094317
Harvey Bastidas <harveybc@users.noreply.github.com>
2025-08-25 19:27:10 -0500

    update

---
+++ examples/scripts/run_phase_2b_daily.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+CONFIG_DIR="examples/config/phase_2b_daily"
+
+for file in "$CONFIG_DIR"/*.json; do
+    echo "Running preprocessor with configuration: $(basename "$file")"
+    sh ./predictor.sh --load_config "$file"
+done
+
+
+echo "All configurations processed."
