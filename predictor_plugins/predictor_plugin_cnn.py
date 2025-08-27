096a587d9ebb0bfc7c89d64be9d05a6f0088506b
Harvey Bastidas <harveybc@users.noreply.github.com>
2025-08-27 13:42:51 -0500

    Update predictor_plugin_cnn.py

--- predictor_plugins/predictor_plugin_cnn.py
+++ predictor_plugins/predictor_plugin_cnn.py
@@ -451,14 +451,14 @@ class Plugin:
                 name="conv_merged_features_1"
             )(inputs)
             
-            merged = Conv1D(
-                filters=branch_units,
-                kernel_size=3,
-                strides=2,
-                padding='same',
-                activation=activation,
-                name="conv_merged_features_2"
-            )(merged)
+            #merged = Conv1D(
+            #    filters=branch_units,
+            #    kernel_size=3,
+            #    strides=2,
+            #    padding='same',
+            #    activation=activation,
+            #    name="conv_merged_features_2"
+            #)(merged)
             
         # --- Heads ---
         outputs_list = []
@@ -467,11 +467,11 @@ class Plugin:
             branch_suffix = f"_h{horizon}"
             #optional conv1d head layers
             xh = Conv1D(filters=branch_units, kernel_size=3, strides=2, padding='valid', kernel_regularizer=l2(l2_reg), name=f"conv1d_1{branch_suffix}")(merged)
-            xh = Conv1D(filters=lstm_units, kernel_size=3, strides=2, padding='valid', kernel_regularizer=l2(l2_reg), name=f"conv1d_2{branch_suffix}")(xh)
+            #xh = Conv1D(filters=lstm_units, kernel_size=3, strides=2, padding='valid', kernel_regularizer=l2(l2_reg), name=f"conv1d_2{branch_suffix}")(xh)
             # batch normalization
             #xh = BatchNormalization(name=f"batch_norm_1{branch_suffix}")(xh)
             # optional bidir return_sequences=True layers
-            xh = Bidirectional(LSTM(lstm_units, return_sequences=True), name=f"bidir_lstm_o_{branch_suffix}")(xh)
+            #xh = Bidirectional(LSTM(lstm_units, return_sequences=True), name=f"bidir_lstm_o_{branch_suffix}")(xh)
             # mandatory lstm output layer
             lstm_output = Bidirectional(LSTM(lstm_units, return_sequences=False), name=f"bidir_lstm{branch_suffix}")(xh)
             # optional output dense layers
