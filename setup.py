469c3a7758e8048354f988c8c9a63b613a880e1c
Harvey Bastidas <harveybc@users.noreply.github.com>
2026-01-20 11:01:17 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ setup(
             'stl_mimo=predictor_plugins.stl_mimo_predictor:Plugin',
             'prophet=predictor_plugins.predictor_plugin_prophet:Plugin',
             'tft=predictor_plugins.predictor_plugin_tft:Plugin',
-            'base=predictor_plugin.predictor_plugin_base:Plugin'
+            'base=predictor_plugins.predictor_plugin_base:Plugin'
         ],
         # Plugins para la Optimización (por defecto, basado en DEAP)
         'optimizer.plugins': [
